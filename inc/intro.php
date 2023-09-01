494cca031e6a4df868740d6ee3959e309fbd96ea
DESKTOP-SK1K6UJ\ori <ori.sarah.sa@gmail.com>
2023-09-01 17:08:01 +0300

    setup project

---
+++ inc/intro.php
@@ -0,0 +1,119 @@
+<?php
+
+function styled_var_dump($data) { 
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';  
+}
+
+function create_dir($dir) {
+    if (!is_dir($dir) && !mkdir($dir, 0777, true)){
+        exit("Error creating folder $dir");
+    }
+}
+
+function get_ext($name) {
+    $n = strrpos($name, '.');
+    return ($n === false) ? '' : substr($name, $n+1);
+}
+
+function get_filename_without_ext($filename) {
+    return preg_replace("/\.[^.]+$/", "", $filename);
+}
+
+function createImageFrom($type, $targetPath) {
+    switch($type){ 
+        case 'jpg':
+            $im = imagecreatefromjpeg($targetPath); 
+            break; 
+        case 'jpeg': 
+            $im = imagecreatefromjpeg($targetPath); 
+            break; 
+        case 'png': 
+            $im = imagecreatefrompng($targetPath); 
+            break; 
+        default: 
+            $im = imagecreatefromjpeg($targetPath); 
+    } 
+
+    return $im;
+}
+
+// post form
+if (isset($_POST['submit'])) {
+    $targetDir = dirname(__DIR__) . '/uploads/';  
+    create_dir($targetDir);
+
+    $upload_image = $_FILES['upload_image'];
+    $upload_watermark = $_FILES['upload_watermark'];
+
+    if ($upload_image['error'] === 4 || $upload_watermark['error'] == 4) {
+        $error = 'תמונה לא הועלתה או סימן מים לא הועלה.';
+    }
+    if ($upload_image['error'] !== 0 || $upload_watermark['error'] !== 0)
+    {
+        $error = 'תמונה לא הועלתה או סימן מים לא הועלה.';
+    }
+    else {
+        // image upload path 
+        $fileName = basename($upload_image["name"]);  
+        $fileName = get_filename_without_ext($fileName) . '_' . time() . '.' . get_ext($fileName);
+        $targetImagePath = $targetDir . $fileName; 
+        $imageType = pathinfo($targetImagePath, PATHINFO_EXTENSION);
+
+        // watermark upload path
+        $fileName = basename($upload_watermark["name"]); 
+        $targetWatermarkPath = $targetDir . $fileName; 
+        $watermarkType = pathinfo($targetWatermarkPath, PATHINFO_EXTENSION);
+
+        // Allow certain file formats 
+        $allowTypes = ['jpg','jpeg','png', 'jfif'];
+        if(in_array($imageType, $allowTypes) && in_array($watermarkType, $allowTypes)) {
+            // Upload file to the server 
+            if(move_uploaded_file($upload_image["tmp_name"], $targetImagePath)
+                    && move_uploaded_file($upload_watermark["tmp_name"], $targetWatermarkPath)) { 
+                $im = createImageFrom($imageType, $targetImagePath);
+                $wt = createImageFrom($watermarkType, $targetWatermarkPath);
+
+                // Set the margins for the watermark 
+                $marge_right = 0; 
+                $marge_bottom = 0; 
+                 
+                // Get the height/width of the watermark image 
+                $sx = imagesx($wt); 
+                $sy = imagesy($wt); 
+                 
+                // Copy the watermark image onto our photo using the margin offsets and  
+                // the photo width to calculate the positioning of the watermark. 
+                imagecopy(
+                    $im, 
+                    $wt, 
+                    imagesx($im) / 2 - $sx / 2 - $marge_right, 
+                    imagesy($im) / 2 - $sy / 2 - $marge_bottom, 
+                    0, 
+                    0, 
+                    imagesx($wt), 
+                    imagesy($wt)
+                ); 
+                 
+                // Save image and free memory 
+                imagepng($im, $targetImagePath); 
+                imagedestroy($im);
+
+                if (file_exists($targetImagePath)){ 
+                    $successMsg = "התמונה עם סימני המים נוצרה בהצלחה."; 
+                } else { 
+                    $errorMsg = "תהליך יירת התמונה כשל, נא נסה שנית."; 
+                }  
+
+                if (file_exists($targetWatermarkPath)) {
+                    unlink($targetWatermarkPath);
+                }
+            } else { 
+                $errorMsg = "אירעה טעות בהעלאת התמונה."; 
+            }       
+        } else{ 
+            $errorMsg = 'ניתן להעלות רק את הפורמטים הבאים: JPG, JPEG, ו- PNG.'; 
+        } 
+    }
+}
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+ob_start();
+
+require_once __DIR__ . '/./vendor/autoload.php';
+require_once __DIR__ . '/./inc/intro.php';
+?>
+
+<!DOCTYPE html>
+<html lang="he" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>מחולל סימני מים בתמונה</title>
+
+    <!-- Bootstrap core-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 mx-auto">
+                <form action="" method="post"  enctype="multipart/form-data">
+                    <h1 class="mb-3 text-primary">מחולל סימני מים בתמונה</h1>
+
+                    <?php if (isset($errorMsg) && $errorMsg) { ?>
+                        <div class="alert alert-danger"><?= $errorMsg ?></div>
+                    <?php } else if (isset($successMsg) && $successMsg) { ?>
+                        <div class="alert alert-success"><?= $successMsg ?></div>
+                    <?php } ?>
+                    <div class="mb-3">
+                        <label for="upload-image">תמונה</label>
+                        <input type="file" class="form-control" id="upload-image" name="upload_image" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="upload-watermark">סימן מים</label>
+                        <input type="file" class="form-control" id="upload-watermark" name="upload_watermark" />
+                    </div>
+
+                    <button type="submit" name="submit" class="btn btn-primary">שלח</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+     <!-- Jquery -->
+     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Bootstrap 5 -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+<?php
+ob_end_flush();
